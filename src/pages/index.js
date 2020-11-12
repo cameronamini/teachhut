5aa6e1a8ccff2480b30e98cc8655aa24ad6caff6
Cameron Amini <cameron.h.amini@gmail.com>
2020-11-12 10:51:17 -0500

    added images and video

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Hero from '../components/Hero'
+import InfoSection from '../components/InfoSection'
+import InfoSection2 from '../components/InfoSection2'
+import InfoSection3 from '../components/InfoSection3'
+
+const Home = () => {
+    const [sidebarOpen, setsidebarOpen] = useState(false)
+
+    const openSidebar = () => {
+      setsidebarOpen(true)
+    }
+    const closeSidebar = () => {
+      setsidebarOpen(false)
+    }
+  
+  
+    return (
+        <>
+          <Navbar openSidebar={openSidebar}/> 
+          <Sidebar closeSidebar={closeSidebar} sidebarOpen={sidebarOpen}/>
+          <Hero/>
+          <InfoSection/>
+          <InfoSection2/>
+          <InfoSection3/>
+        </>
+        )
+}
+
+export default Home
